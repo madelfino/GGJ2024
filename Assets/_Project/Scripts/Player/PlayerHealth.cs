7cf261835c2668dfcf4c96e79d9feb2885313d5d
Michael Delfino <michael.a.delfino@gmail.com>
2024-01-28 14:04:29 +0700

    Add train and character to level 1

--- Assets/_Project/Scripts/Player/PlayerHealth.cs
+++ Assets/_Project/Scripts/Player/PlayerHealth.cs
@@ -7,13 +7,20 @@ public class PlayerHealth : Observer
     public int Health => health;
     private int health = 2;
     [SerializeField] Slider _painScaleSlider;
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip loudFartClip;
 
     public override void ReceiveSignal(SubjectOfObserver subject)
     {
         if (_painScaleSlider.value >= 0.7f)
         {
+            Debug.Log("Player Hurt!");
             health--;
             //pop up shit screen
+
+            if (audioSource != null && loudFartClip != null) {
+                audioSource.PlayOneShot(loudFartClip);
+            }
         }
     }
 }
